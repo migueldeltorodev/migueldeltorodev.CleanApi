ef831019df4359ab4cfc1d90338bf99a8ed109b6
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-13 17:17:18 -0500

    feat (DeleteCustomerEndpoint): created a new endpoint to delete a customer

---
+++ Customers.Api/Endpoints/DeleteCustomerEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Customers.Api.Services;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Customers.Api.Endpoints
+{
+    [HttpDelete("customers/{id:guid}"), AllowAnonymous]
+    public class DeleteCustomerEndpoint(ICustomerService _customerService) : EndpointWithoutRequest
+    {
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            var id = Route<Guid>("id");
+
+            var deleted = await _customerService.DeleteAsync(id);
+            if (!deleted)
+            {
+                await SendNotFoundAsync(ct);
+                return;
+            }
+
+            await SendNoContentAsync(ct);
+        }
+    }
+}
