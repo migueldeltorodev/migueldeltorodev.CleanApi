a38bb0113d8e8ef65cd7397198f2a33af2bb8337
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-13 17:32:38 -0500

    build (Validation): created new validation rules with FluentValidation

---
+++ Customers.Api/Validation/ValidationExceptionMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Customers.Api.Contracts.Responses;
+using FluentValidation;
+
+namespace Customers.Api.Validation
+{
+    public class ValidationExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ValidationExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ValidationException exception)
+            {
+                context.Response.StatusCode = 400;
+                var validationFailureResponse = new ValidationFailureResponse
+                {
+                    Errors = exception.Errors.Select(x => x.ErrorMessage).ToList()
+                };
+                await context.Response.WriteAsJsonAsync(validationFailureResponse);
+            }
+        }
+    }
+}
