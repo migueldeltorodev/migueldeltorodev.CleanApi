48bc29e4040b9abf5a30be8c1f9479b1db8d5f58
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-13 15:48:08 -0500

    build (Database): created new database schema with sqlite

---
+++ Customers.Api/Database/SqliteConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace Customers.Api.Database
+{
+    public class SqliteConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqliteConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IDbConnection> CreateConnectionAsync()
+        {
+            var connection = new SqliteConnection(_connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
