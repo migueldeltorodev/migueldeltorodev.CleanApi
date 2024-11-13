b2f3ab4404542b0f4c1193ea11163a61aea6ef43
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-13 17:09:27 -0500

    feat (CreateCustomerEndpoint): created a new endpoint to create a new customer

---
+++ Customers.Api/Endpoints/CreateCustomerEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using Customers.Api.Mapping;
+using Customers.Api.Services;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Customers.Api.Endpoints
+{
+    [HttpPost("customers"), AllowAnonymous]
+    public class CreateCustomerEndpoint(ICustomerService _customerService) : Endpoint<CreateCustomerRequest, CustomerResponse>
+    {
+        public override async Task HandleAsync(CreateCustomerRequest req, CancellationToken ct)
+        {
+            var customer = req.ToCustomer();
+
+            await _customerService.CreateAsync(customer);
+
+            var response = customer.ToCustomerResponse();
+            await SendCreatedAtAsync<GetCustomerEndpoint>(
+                new { Id = customer.Id },
+                response,
+                generateAbsoluteUrl: true,
+                cancellation: ct);
+        }
+    }
+}
