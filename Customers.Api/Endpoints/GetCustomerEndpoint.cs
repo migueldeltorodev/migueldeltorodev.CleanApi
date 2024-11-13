ccd9f0656da1003f3a1779fdf07020e0c44eeb96
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-13 17:09:06 -0500

    feat (GetCustomerEndpoint): created new endpoint to Get a Customer

---
+++ Customers.Api/Endpoints/GetCustomerEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Customers.Api.Contracts.Responses;
+using Customers.Api.Mapping;
+using Customers.Api.Services;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Customers.Api.Endpoints
+{
+    [HttpGet("customers/{id:guid}"), AllowAnonymous]
+    public class GetCustomerEndpoint(ICustomerService _customerService) : EndpointWithoutRequest<CustomerResponse>
+    {
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            var id = Route<Guid>("id");
+
+            var customer = await _customerService.GetAsync(id);
+            if (customer is null)
+            {
+                await SendNotFoundAsync(ct);
+                return;
+            }
+
+            var response = customer.ToCustomerResponse();
+            await SendOkAsync(response, ct);
+        }
+    }
+}
