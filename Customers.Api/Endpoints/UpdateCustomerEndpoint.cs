8c65afd36ff36846e3d67e6a6e70d4fd926f20f0
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-13 17:16:59 -0500

    feat (UpdateCustomerEndpoint): created new endpoint to update a customer data

---
+++ Customers.Api/Endpoints/UpdateCustomerEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using Customers.Api.Mapping;
+using Customers.Api.Services;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Customers.Api.Endpoints
+{
+    [HttpPut("customers/{id:guid}"), AllowAnonymous]
+    public class UpdateCustomerEndpoint(ICustomerService _customerService) : Endpoint<UpdateCustomerRequest, CustomerResponse>
+    {
+        public override async Task HandleAsync(UpdateCustomerRequest req, CancellationToken ct)
+        {
+            var customer = req.ToCustomer();
+
+            var updated = await _customerService.UpdateAsync(customer);
+            if (!updated)
+            {
+                await SendNotFoundAsync(ct);
+                return;
+            }
+
+            var response = customer.ToCustomerResponse();
+            await SendOkAsync(response, ct);
+        }
+    }
+}
