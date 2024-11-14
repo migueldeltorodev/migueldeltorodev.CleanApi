e87b3f555bcf56f9fe977ac21c94b62250335809
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-11-13 23:13:03 -0500

    build (Summaries): created all summaries for all endpoints

---
+++ Customers.Api/Summaries/DeleteCustomerSummary.cs
@@ -0,0 +1,16 @@
+﻿using Customers.Api.Endpoints;
+using FastEndpoints;
+
+namespace Customers.Api.Summaries
+{
+    public class DeleteCustomerSummary : Summary<DeleteCustomerEndpoint>
+    {
+        public DeleteCustomerSummary()
+        {
+            Summary = "Deleted a customer the system";
+            Description = "Deleted a customer the system";
+            Response(204, "The customer was deleted successfully");
+            Response(404, "The customer was not found in the system");
+        }
+    }
+}
